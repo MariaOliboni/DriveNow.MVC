4bb651b3f55def8281c97cc4bf995ab2e9aea2a4
MariaOliboni <maria.e.santos387@aluno.senai.br>
2026-04-08 10:37:36 -0300

    adiciona a model agencia

---
+++ Models/AgenciaView.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
+
+namespace DriveNow.MVC.Models
+{
+    public class AgenciaView
+    {
+        public int Id { get; set; }
+        public string NomeFantasia { get; set; }
+        public string Cep { get; set; }
+        [BindNever]
+        public string? Logradouro { get; set; }
+        [BindNever]
+        public string? Bairro { get; set; }
+        [BindNever]
+        public string? Localidade { get; set; }
+        [BindNever]
+        public string? Uf { get; set; }
+
+
+    }
+}
